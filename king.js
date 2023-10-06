02ebde9962712c0abf7cdcb206b77400202369d7
moabdelaziem123 <144205494+moabdelaziem123@users.noreply.github.com>
2023-10-06 14:53:18 +0300

    Add files via upload

---
+++ king.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+
+menu.onclick = function(){
+menu.classList.toggle('bx-x');
+navbar.classList.toggle('open');
+
+
+};
